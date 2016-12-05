641fb9d1b19c85249caa145034da4fb65010d8d9
Test developer <test@testmail.com>
2016-12-05 16:48:36 +0530

    Now logic is implementated and refactoring of code is also done

--- preno/src/main/java/com/tdd/practices/NumberChecker.java
+++ preno/src/main/java/com/tdd/practices/NumberChecker.java
@@ -2,9 +2,23 @@ package com.tdd.practices;
 
 public class NumberChecker {
 
-	public Boolean isPrefectNumber(int i) {
-		// TODO Auto-generated method stub
-		return true;
+	public Boolean isPrefectNumber(int number) {
+		boolean isPerfect = false;
+		if (number > 1) {
+			int temp = 0;
+			for (int i = 1; i <= number / 2; i++) {
+				if (number % i == 0) {
+					temp += i;
+				}
+			}
+			if (temp == number) {
+				isPerfect = true;
+			} else {
+				isPerfect = false;
+			}
+		}
+
+		return isPerfect;
 	}
 
 }
