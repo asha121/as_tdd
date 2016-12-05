41baeb15279f7e2cd2ffa76f2d2b63452c8c4d8b
Test developer <test@testmail.com>
2016-12-05 17:08:51 +0530

    code refactoring

--- preno/src/main/java/com/tdd/practices/NumberChecker.java
+++ preno/src/main/java/com/tdd/practices/NumberChecker.java
@@ -17,7 +17,6 @@ public class NumberChecker {
 				isPerfect = false;
 			}
 		}
-
 		return isPerfect;
 	}
 
--- preno/src/test/java/com/tdd/practices/NumberCheckerTest.java
+++ preno/src/test/java/com/tdd/practices/NumberCheckerTest.java
@@ -1,42 +1,54 @@
 package com.tdd.practices;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import junit.framework.Assert;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = NumberChecker.class, loader = AnnotationConfigContextLoader.class)
 public class NumberCheckerTest {
+	@Autowired
+	private NumberChecker numChecker;
 
 	@Test
 	public void testIs_6_PrefectNumber() {
-		Assert.assertEquals(true, new NumberChecker().isPrefectNumber(6).booleanValue());
+		Assert.assertEquals(true, numChecker.isPrefectNumber(6).booleanValue());
 
 	}
+
 	@Test
 	public void testIs_28_PrefectNumber() {
-		Assert.assertEquals(true, new NumberChecker().isPrefectNumber(28).booleanValue());
+		Assert.assertEquals(true, numChecker.isPrefectNumber(28).booleanValue());
 
 	}
+
 	@Test
 	public void testIs_496_PrefectNumber() {
-		Assert.assertEquals(true, new NumberChecker().isPrefectNumber(496).booleanValue());
+		Assert.assertEquals(true, numChecker.isPrefectNumber(496).booleanValue());
 
 	}
+
 	@Test
 	public void testIs_8128_PrefectNumber() {
-		Assert.assertEquals(true, new NumberChecker().isPrefectNumber(496).booleanValue());
+		Assert.assertEquals(true, numChecker.isPrefectNumber(496).booleanValue());
 
 	}
-	
+
 	@Test
 	public void testIs_negative_NumberCheck() {
-		Assert.assertNotSame(true, new NumberChecker().isPrefectNumber(-28).booleanValue());
+		Assert.assertNotSame(true, numChecker.isPrefectNumber(-28).booleanValue());
+		Assert.assertSame(false, numChecker.isPrefectNumber(-28).booleanValue());
 
 	}
+
 	@Test
 	public void test_to_check_fail_NumberCheck() {
-		Assert.assertNotSame(true, new NumberChecker().isPrefectNumber(10).booleanValue());
+		Assert.assertNotSame(true, numChecker.isPrefectNumber(10).booleanValue());
+		Assert.assertSame(false, numChecker.isPrefectNumber(10).booleanValue());
 
 	}
 
