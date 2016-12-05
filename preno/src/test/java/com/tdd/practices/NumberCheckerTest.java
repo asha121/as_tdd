789143d1c3daac90ce7f37662e875d7bea32f884
Test developer <test@testmail.com>
2016-12-05 16:45:01 +0530

    testcases for negative secarnio are failing and it is the time to implement the logic

--- preno/src/test/java/com/tdd/practices/NumberCheckerTest.java
+++ preno/src/test/java/com/tdd/practices/NumberCheckerTest.java
@@ -13,5 +13,31 @@ public class NumberCheckerTest {
 		Assert.assertEquals(true, new NumberChecker().isPrefectNumber(6).booleanValue());
 
 	}
+	@Test
+	public void testIs_28_PrefectNumber() {
+		Assert.assertEquals(true, new NumberChecker().isPrefectNumber(28).booleanValue());
+
+	}
+	@Test
+	public void testIs_496_PrefectNumber() {
+		Assert.assertEquals(true, new NumberChecker().isPrefectNumber(496).booleanValue());
+
+	}
+	@Test
+	public void testIs_8128_PrefectNumber() {
+		Assert.assertEquals(true, new NumberChecker().isPrefectNumber(496).booleanValue());
+
+	}
+	
+	@Test
+	public void testIs_negative_NumberCheck() {
+		Assert.assertNotSame(true, new NumberChecker().isPrefectNumber(-28).booleanValue());
+
+	}
+	@Test
+	public void test_to_check_fail_NumberCheck() {
+		Assert.assertNotSame(true, new NumberChecker().isPrefectNumber(10).booleanValue());
+
+	}
 
 }
