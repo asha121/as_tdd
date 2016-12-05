08f93b718f2f52785da350e6ebbfabfad79e7637
Test developer <test@testmail.com>
2016-12-05 19:27:09 +0530

    refactor for the testcase

--- preno/src/main/java/com/tdd/practices/Game.java
+++ preno/src/main/java/com/tdd/practices/Game.java
@@ -1,13 +1,38 @@
 package com.tdd.practices;
 
+import com.tdd.practices.rps.Paper;
 import com.tdd.practices.rps.Player;
 import com.tdd.practices.rps.Rock;
 
 public class Game {
 
-	public Player WinnerBetween(String string, String string2) {
-		// TODO Auto-generated method stub
-		return (Player) new Rock("Rock","rock crushes scissors -> rock wins");
+	public Player WinnerBetween(String fPlayer, String sPlayer) {
+		Player player = null;
+		if (fPlayer.equals(sPlayer)) {
+			player = samePlayer(sPlayer, player);
+		} else if ((fPlayer.equals("Rock") && sPlayer.equals("Scissor"))
+				|| (sPlayer.equals("Rock") && fPlayer.equals("Scissor"))) {
+			player = new Rock("Rock", "rock crushes scissors -> rock wins");
+		} else if ((fPlayer.equals("Rock") && sPlayer.equals("Paper"))
+				|| (sPlayer.equals("Rock") && fPlayer.equals("Paper"))) {
+			player = new Paper("Paper", "paper covers rock -> paper wins");
+		} else if ((fPlayer.equals("Scissor") && sPlayer.equals("Paper"))
+				|| (sPlayer.equals("Scissor") && fPlayer.equals("Paper"))) {
+			player = new Scissor("Scissor", "scissors cut paper -> scissors wins");
+		}
+		return player;
+
+	}
+
+	private Player samePlayer(String sPlayer, Player player) {
+		if (sPlayer.equals("Rock")) {
+			player = new Rock("Rock", "TIE");
+		} else if (sPlayer.equals("Scissor")) {
+			player = new Scissor("Scissor", "TIE");
+		} else if (sPlayer.equals("Paper")) {
+			player = new Paper("Paper", "TIE");
+		}
+		return player;
 	}
 
 }
--- preno/src/test/java/com/tdd/practices/GameTest.java
+++ preno/src/test/java/com/tdd/practices/GameTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
+import com.tdd.practices.rps.Paper;
 import com.tdd.practices.rps.Player;
 import com.tdd.practices.rps.Rock;
 
@@ -18,11 +19,10 @@ public class GameTest {
 
 	@Autowired
 	Game game;
-	
 
 	@Test
 	public void testRockCrushScissor() {
-		Player rock= new Rock("Rock","rock crushes scissors -> rock wins");
+		Player rock = new Rock("Rock", "rock crushes scissors -> rock wins");
 		Assert.assertEquals(rock.getMsg(), (game.WinnerBetween("Rock", "Scissor")).getMsg());
 		Assert.assertEquals(rock.getName(), (game.WinnerBetween("Rock", "Scissor")).getName());
 		Assert.assertNotSame("Scissor", game.WinnerBetween("Rock", "Scissor").getName());
@@ -32,27 +32,60 @@ public class GameTest {
 
 	@Test
 	public void testPaperCoversRock() {
-		Assert.assertEquals("paper", game.WinnerBetween("Rock", "Paper"));
+		Player paper = new Paper("Paper", "paper covers rock -> paper wins");
+		Player testPlayer = game.WinnerBetween("Paper", "Rock");
+		Assert.assertEquals(paper.getMsg(), testPlayer.getMsg());
+		Assert.assertEquals(paper.getName(), testPlayer.getName());
+		Assert.assertNotSame("Scissor", testPlayer.getName());
+		Assert.assertNotSame("Scissor", testPlayer.getMsg());
+		Assert.assertSame(paper.getClass(), testPlayer.getClass());
 	}
 
 	@Test
 	public void testScissorsCutPaper() {
-		Assert.assertEquals("scissor", game.WinnerBetween("Scissor", "Paper"));
-	}
 
-	@Test
-	public void testRockTie() {
-		Assert.assertEquals("TIE", game.WinnerBetween("Rock", "Rock"));
-	}
-
-	@Test
-	public void testPaperTie() {
-		Assert.assertEquals("TIE", game.WinnerBetween("Paper", "Paper"));
+		Player scissor = new Scissor("Scissor", "scissors cut paper -> scissors wins");
+		Player testPlayer = game.WinnerBetween("Scissor", "Paper");
+		Assert.assertEquals(scissor.getMsg(), testPlayer.getMsg());
+		Assert.assertEquals(scissor.getName(), testPlayer.getName());
+		Assert.assertNotSame("Paper", testPlayer.getName());
+		Assert.assertNotSame("Paper", testPlayer.getMsg());
+		Assert.assertSame(scissor.getClass(), testPlayer.getClass());
 	}
 
 	@Test
 	public void testScissorTie() {
-		Assert.assertEquals("TIE", game.WinnerBetween("Scissor", "Scissor"));
+		Player tieScissor = new Scissor("Scissor", "TIE");
+		Player testPlayer = game.WinnerBetween("Scissor", "Scissor");
+		Assert.assertEquals(tieScissor.getMsg(), testPlayer.getMsg());
+		Assert.assertEquals(tieScissor.getName(), testPlayer.getName());
+		Assert.assertNotSame("Paper", testPlayer.getName());
+		Assert.assertNotSame("Rock", testPlayer.getName());
+		Assert.assertNotSame("scissors cut paper -> scissors wins", testPlayer.getMsg());
+	}
+
+	@Test
+	public void testPaperTie() {
+		Player tieScissor = new Paper("Paper", "TIE");
+		Player testPlayer = game.WinnerBetween("Paper", "Paper");
+		Assert.assertEquals(tieScissor.getMsg(), testPlayer.getMsg());
+		Assert.assertEquals(tieScissor.getName(), testPlayer.getName());
+		Assert.assertNotSame("Scissor", testPlayer.getName());
+		Assert.assertNotSame("Rock", testPlayer.getName());
+		Assert.assertEquals("TIE", testPlayer.getMsg());
+	}
+
+	@Test
+	public void testRockTie() {
+		Player tieScissor = new Paper("Rock", "TIE");
+		Player testPlayer = game.WinnerBetween("Rock", "Rock");
+		Assert.assertEquals(tieScissor.getMsg(), testPlayer.getMsg());
+		Assert.assertEquals(tieScissor.getName(), testPlayer.getName());
+		Assert.assertNotSame("Scissor", testPlayer.getName());
+		Assert.assertNotSame("Paper", testPlayer.getName());
+		Assert.assertNotSame("scissors cut paper -> scissors wins", testPlayer.getMsg());
+		Assert.assertEquals("TIE", testPlayer.getMsg());
+
 	}
 
 }
