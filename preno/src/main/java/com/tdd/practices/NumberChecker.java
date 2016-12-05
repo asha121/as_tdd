e4e92842029de1471fd51714b1419f90062a9a6f
Test developer <test@testmail.com>
2016-12-05 16:32:58 +0530

    Creating implementation class from test case

---
+++ preno/src/main/java/com/tdd/practices/NumberChecker.java
@@ -0,0 +1,10 @@
+package com.tdd.practices;
+
+public class NumberChecker {
+
+	public Boolean isPrefectNumber(int i) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
--- preno/src/test/java/com/tdd/practices/NumberCheckerTest.java
+++ preno/src/test/java/com/tdd/practices/NumberCheckerTest.java
@@ -4,13 +4,14 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 public class NumberCheckerTest {
 
 	@Test
-	public void testIsPrefectNumber() {
-		NumberChecker numChecker = new NumberChecker();
-		numChecker.isPrefectNumber(6);
-		fail("Not yet implemented");
+	public void testIs_6_PrefectNumber() {
+		Assert.assertEquals(true, new NumberChecker().isPrefectNumber(6).booleanValue());
+
 	}
 
 }
