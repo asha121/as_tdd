7223179dccce0e25b67411b133b37abc5077c974
Test developer <test@testmail.com>
2016-12-05 16:24:01 +0530

    skeleton project

---
+++ preno/src/test/java/com/tdd/practices/NumberCheckerTest.java
@@ -0,0 +1,14 @@
+package com.tdd.practices;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class NumberCheckerTest {
+
+	@Test
+	public void testIsPrefectNumber() {
+		fail("Not yet implemented");
+	}
+
+}
