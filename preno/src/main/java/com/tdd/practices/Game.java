f539cd3617f75d2bd9da0ac40b5155616dd5797c
Test developer <test@testmail.com>
2016-12-05 17:59:51 +0530

    refactoring and implement the code to pass the first test case

---
+++ preno/src/main/java/com/tdd/practices/Game.java
@@ -0,0 +1,10 @@
+package com.tdd.practices;
+
+public class Game {
+
+	public String WinnerBetween(String string, String string2) {
+		// TODO Auto-generated method stub
+		return "Rock";
+	}
+
+}
--- preno/src/test/java/com/tdd/practices/GameTest.java
+++ preno/src/test/java/com/tdd/practices/GameTest.java
@@ -2,7 +2,14 @@ package com.tdd.practices;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Game.class, loader = AnnotationConfigContextLoader.class)
 
 public class GameTest {
 
